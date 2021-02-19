c6b8d979d9f2bcc3ec96425e1635d00799beebb4
unknown <erickvillegasecheverria@gmail.com>
2021-02-18 20:03:33 -0800

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,36 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    item.enhancement += 1;
+    return { ...item };
+  } else {
+    return { ...item };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    item.durability = item.durability - 5;
+    return { ...item };
+  } else if (item.enhancement >= 15) {
+    item.durability = item.durability - 10;
+    if (item.enhancement > 16) {
+      item.enhancement = item.enhancement - 1;
+      return { ...item };
+    } else {
+      return { ...item };
+    }
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  if (item.duravility < 100) {
+    item.duravility = 100;
+    return { ...item };
+  } else {
+    return { ...item };
+  }
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,60 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+const items = [
+  {
+    name: "excalibur",
+    durability: 100,
+    enhancement: 20,
+  },
+  {
+    name: "long sword",
+    durability: 15,
+    enhancement: 0,
+  },
+  {
+    name: "gorgon shield",
+    durability: 85,
+    enhancement: 10,
+  },
+  {
+    name: "wood bow",
+    durability: 25,
+    enhancement: 0,
+  },
+];
+
+describe("enhancer test suit", () => {
+  test("enhancer success at max~!", () => {
+    let expected_value = {
+      name: "excalibur",
+      durability: 100,
+      enhancement: 20,
+    };
+    expect(enhancer.success(items[0])).toEqual(expected_value);
+  });
+  test("enhancer success~!", () => {
+    let expected_value = {
+      name: "long sword",
+      durability: 15,
+      enhancement: 1,
+    };
+    expect(enhancer.success(items[1])).toEqual(expected_value);
+  });
+  test("enhancer failes at max~!", () => {
+    let expected_value = {
+      name: "excalibur",
+      durability: 90,
+      enhancement: 19,
+    };
+    expect(enhancer.fail(items[0])).toEqual(expected_value);
+  });
+  test("enhancer failes at max~!", () => {
+    let expected_value = {
+      name: "gorgon shield",
+      durability: 80,
+      enhancement: 10,
+    };
+    expect(enhancer.fail(items[2])).toEqual(expected_value);
+  });
+});
